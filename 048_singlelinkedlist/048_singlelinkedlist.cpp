e7729a14897abd9f0b2d65b8bf675d80fdd366cb
rafiazharr17 <rafipilo17@gmail.com>
2023-05-18 18:42:18 +0700

    memanggil method addNode dalam case 1, method listEmpty dan deleteNode dalam case 2, method traverse dalam case 3, method searchData dalam case 4

--- 048_singlelinkedlist/048_singlelinkedlist.cpp
+++ 048_singlelinkedlist/048_singlelinkedlist.cpp
@@ -144,7 +144,47 @@ int main() {
 			cout << "pilihan: ";
 			cin >> pilihan;
 
+			switch (pilihan) {
+			case 1:
+				addNode();
+				cout << "Data Berhasil Ditambahkan" << endl;
+				system("pause");
+				system("cls");
+				break;
 
+			case 2:
+				if (listEmpty())
+				{
+					cout << "List Kosong" << endl;
+					system("pause");
+					system("cls");
+					break;
+				}
+
+				int nim;
+				cout << "masukan NIM: ";
+				cin >> nim;
+				if (deleteNode(nim)) {
+					cout << "nim: " << nim << "berhasil dihapus" << endl;
+					system("pause");
+					system("cls");
+				}
+				else
+					cout << "Data tidak ditemukan" << endl;
+				break;
+			case 3:
+				traverse();
+				break;
+			case 4:
+				searchData();
+				break;
+			case 5:
+				break;
+			default:
+				cout << "pilihan tidak ada" << endl;
+				break;
+			}
 		}
+
 	}
 }
